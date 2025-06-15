068f7c4e0ea94bb5f0bc3874916684e2dd6f3d51
Mark Harfouche <mark.harfouche@gmail.com>
2025-06-15 08:56:00 -0400

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,13 +4,15 @@ setlocal enabledelayedexpansion
 mkdir build
 cd build
 
-cmake -LAH -G "Ninja"                                                     ^
-    -DCMAKE_BUILD_TYPE="Release"                                          ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%                               ^
-    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%                                  ^
-    -DBUILD_SHARED_LIBS=1                                                 ^
+cmake -LAH -G "Ninja"                          ^
+    %CMAKE_ARGS%                               ^
+    -DCMAKE_BUILD_TYPE="Release"               ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%    ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%       ^
+    -DBUILD_SHARED_LIBS=1                      ^
+    -DBUILD_STATIC_LIBS=OFF                    ^
+    -DBUILD_SHARED_LIBS=ON                     ^
     ..
-
 if errorlevel 1 exit 1
 
 ninja install
